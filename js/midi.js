dd946c619a09d5e3799e165a76f3b11c95b3ac12
ljssgit <87521738+ljssgit@users.noreply.github.com>
2021-12-31 01:50:24 +0900

    Update midi.js

--- js/midi.js
+++ js/midi.js
@@ -16,16 +16,17 @@ class MIDI {
         let ratio = 0.7
         for (let i in sizes) sizes[i] = sizes[i]*ratio
 
-        let kbd = JZZ.input.Kbd({at:'piano', from:'C3', to:'B3',
+        let kbd = JZZ.input.Kbd({at:'piano', from:'C3', to:'B5',
             wl:sizes[0], ww:sizes[1], bl:sizes[2], bw:sizes[3],
             0:   { pos: 'W' },
             320: { },
-            450: { to: 'E5' },
-            610: { to: 'B5' },
+            //450: { to: 'E5' },
+            //610: { to: 'B5' },
             650: { to: 'E6' },
             900: { to: 'B6' },
-            1200: { to: 'B7' },
-            1500: { to: 'B8' },
+            1200: { from: 'C2' },
+            //1200: { to: 'B7' },
+            1500: { to: 'B7' },
             // onCreate: function() {
             //     this.getKeys().setStyle({ margin:0, borderColor:'#00f' });
             //     this.getBlackKeys().setStyle({ backgroundColor:'#c0c' }, {});
