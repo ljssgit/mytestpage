c30d2d9034f389db0a5a50f3e8d20037e972c879
ljssgit <87521738+ljssgit@users.noreply.github.com>
2022-01-26 03:46:24 +0900

    Add files via upload

--- settings.js
+++ settings.js
@@ -7,6 +7,7 @@ class GlobalVar {
     static cur_song_idx = -1;
     static timerid = null;
     static chord_timerid = null;
+    static next_chord_term = false;
 }
 
 Element.prototype.setAttributes = function(obj){ for(var prop in obj) this.setAttribute(prop, obj[prop]) };
